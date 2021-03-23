1633abf70968557916d417b83cb11eed6cb789de
SeungJun <dzzy6505@gmail.com>
2021-03-23 16:44:06 +0900

    add: get API from data parition

--- api.py
+++ api.py
@@ -50,140 +50,8 @@ def querySender(URL, KEY, sigunguCd='36110', bjdongCd='', platGbCd='', bun='', j
     decode_data = response.decode('utf-8')
     xml_parse =  xmltodict.parse(decode_data)
     xml_dict = json.loads(json.dumps(xml_parse))
-
     return xml_dict
 
-# get bjdongCd(법정동) codes
-b_info = pd.read_csv('data/2.세종시_표제부.csv',  dtype={'로트':object, '법정동코드': str})
-law_codes = b_info['법정동코드'].unique().tolist()
-
-#%%
-# get Count Data from API
-count = OrderedDict()
-for law_code in law_codes:
-    bld_info = querySender(URL, KEY, bjdongCd=law_code)
-    totalCount = bld_info['response']['body']['totalCount']
-    count[law_code] = totalCount
-
-
-
-#%%
-# make Columns
-columns = ['대지주소','시군구코드','법정동코드','동명칭','호명칭','층구분코드','층번호','면적(전용면적)','건물명','번','지','주용도코드']
-
-with open('area.csv', "w", encoding='utf-8-sig', newline='') as f:
-    cw = csv.writer(f)
-    cw.writerow(columns)
-
-#%%
-def isNone(item):
-    if not item:
-        return ''
-    else: return str(item)
-
-for law_code in law_codes:
-    # case None of item
-    if not int(count[law_code]):
-        continue
-
-    try:
-        query = querySender(URL, KEY, bjdongCd=law_code, numOfRows=count[law_code])
-    except:
-        print(law_code)
-        continue
-
-    items = query['response']['body']['items']
-
-    for item in items[['items']]:
-        data = [
-            isNone(item['platPlc']),# 대지주소
-            isNone(item['sigunguCd']),# 시군구코드
-            isNone(item['bjdongCd']),# 법정동코드
-            isNone(item['dongNm']),# 동명칭
-            isNone(item['hoNm']),# 호명칭
-            isNone(item['flrGbCd']),# 층구분코드
-            isNone(item['flrNo']),# 층번호
-            isNone(item['area']),# 면적(전용면적)
-            isNone(item['bldNm']),# 건물명
-            isNone(item['bun']),# 번
-            isNone(item['ji']),# 지
-            isNone(item['mainPurpsCdNm']),# 용도코드명
-            isNone(item['mainPurpsCd']),# 주용도코드
-            isNone(item['etcPurps']),# 기타용도
-        ]
-
-
-        with open('area.csv', "a", encoding='utf-8-sig', newline='') as f:
-            cw = csv.writer(f)
-            cw.writerow(data)
-#%%
-# Data List Cannot be Loaded
-df = pd.read_csv('area.csv')
-codes = df['법정동코드'].unique().astype(str).tolist()
-valid_codes = [key for key,cnt  in count.items() if int(cnt)]
-not_valid_codes =[valid_code for valid_code in valid_codes if not valid_code in codes ]
-
-# Data Append
-for law_code in not_valid_codes:
-    data_count = int(count[law_code])
-
-    # case of None in item
-    if not data_count:
-        continue
-
-    # Data Partition
-    numOfRows = (data_count // 4)
-    maxPage = 4 + 1
-
-    for pageNo in tqdm(range(1, maxPage+1)):
-        try:
-            query = querySender(URL, KEY, bjdongCd=law_code, numOfRows=numOfRows, pageNo=pageNo)
-        except:
-            print("Not success : ",law_code)
-            continue
-
-        items = query['response']['body']['items']
-
-        if items:
-            if isinstance(items['item'], dict):
-                data = [
-                    isNone(item['platPlc']),
-                    isNone(item['sigunguCd']),
-                    isNone(item['bjdongCd']),
-                    isNone(item['dongNm']),
-                    isNone(item['hoNm']),
-                    isNone(item['flrGbCd']),
-                    isNone(item['flrNo']),
-                    isNone(item['area']),
-                    isNone(item['bldNm']),
-                    isNone(item['bun']),
-                    isNone(item['ji']),
-                    isNone(item['mainPurpsCdNm'])]
-                with open('area_unloaded.csv', "a", encoding='utf-8-sig', newline='') as f:
-                    cw = csv.writer(f)
-                    cw.writerow(data)
-            else:
-                for item in items['item']:
-                    data = [
-                        isNone(item['platPlc']),
-                        isNone(item['sigunguCd']),
-                        isNone(item['bjdongCd']),
-                        isNone(item['dongNm']),
-                        isNone(item['hoNm']),
-                        isNone(item['flrGbCd']),
-                        isNone(item['flrNo']),
-                        isNone(item['area']),
-                        isNone(item['bldNm']),
-                        isNone(item['bun']),
-                        isNone(item['ji']),
-                        isNone(item['mainPurpsCdNm'])]
-                    with open('area_unloaded.csv', "a", encoding='utf-8-sig', newline='') as f:
-                        cw = csv.writer(f)
-                        cw.writerow(data)
-
-
-
-#%%
 # Write JSON file
 def writeJSON(file_name, data, mode) -> None:
     with open(file_name, mode) as outfile:
@@ -215,5 +83,58 @@ def getDataFromPara(item):
     ]
     return data
 
+def isNone(item):
+    if not item:
+        return ''
+    else: return str(item)
 
-# %%
+
+# get bjdongCd(법정동) codes
+b_info = pd.read_csv('data/2.세종시_표제부.csv',  dtype={'로트':object, '법정동코드': str})
+law_codes = b_info['법정동코드'].unique().tolist()
+
+#%%
+# get Count Data from API
+count = OrderedDict()
+for law_code in law_codes:
+    bld_info = querySender(URL, KEY, bjdongCd=law_code)
+    totalCount = bld_info['response']['body']['totalCount']
+    count[law_code] = totalCount
+
+
+# Columns setting
+columns = ['대지주소', '시군구코드', '법정동코드', '동명칭', '호명칭', '층구분코드', '층번호', '면적(전용면적)', '건물명', '번', '지', '용도코드명', '주용도코드', '기타용도']
+writeCSV(file_name='전유부.csv', data=columns, mode='w')
+
+#%%
+
+for law_code in law_codes:
+    data_count = int(count[law_code])
+
+    # case of None in item
+    if not data_count:
+        continue
+
+    # Data Partition
+    numOfRows = (data_count // 4)
+    maxPage = 4 + 1
+
+    for pageNo in tqdm(range(1, maxPage+1)):
+        try:
+            query = querySender(URL, KEY, bjdongCd=law_code, numOfRows=numOfRows, pageNo=pageNo)
+        except:
+            print("Not success : ",law_code)
+            continue
+
+        items = query['response']['body']['items']
+
+        if items:
+            if isinstance(items['item'], list):
+                for item in items['item']:
+                    data = getDataFromPara(item)
+                    writeCSV('전유부.csv', data, "a")
+            else:
+                data = getDataFromPara(item)
+                writeCSV('전유부.csv', data, "a")
+
+#%%
